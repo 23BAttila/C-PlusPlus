0f94f5cccd5f8d3e0f536ba0336ffc98659ead75
23BAttila <144988273+23BAttila@users.noreply.github.com>
2026-05-15 00:11:14 +0200

    dynamicMem,iterative/recursive

---
+++ 4fourth_round/iterative.cpp
@@ -0,0 +1,17 @@
+//Iterative approach
+
+#include <iostream>
+
+
+void walk(int steps);
+int main(){
+    walk(100);
+
+    return 0;
+}
+void walk(int steps){
+    for (int i = 0; i < steps; i++)
+    {
+        std::cout << "You took a step!\n";
+    } 
+}
---
+++ 4fourth_round/recursive2.cpp
@@ -0,0 +1,22 @@
+//recursive approach
+#include <iostream>
+
+int factorial(int num);
+
+int main(){
+
+    std::cout << factorial(8);
+    
+    return 0;
+}
+
+int factorial(int num){
+
+    if(num > 1)
+    {
+        return num*factorial(num-1);
+    }else
+    {
+        return 1;
+    }
+}
